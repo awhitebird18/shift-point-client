fd50563fd1a500de3ab5483684f769d338e5f4a9
awhitebird18 <aaron.whitebird@gmail.com>
2022-08-01 17:06:18 -0400

    created new dashboard tiles

---
+++ src/Pages/Dashboard/AccountDetails/AccountDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import TileContainer from "../TileLayout/TileLayout";
+
+const AccountDetails = () => {
+  return <TileContainer headerStyles={{ height: "0" }}></TileContainer>;
+};
+
+export default AccountDetails;
